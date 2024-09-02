44c33cae0a7c835a611d3a32a65233024969d98b
maloy-baroi <maloy.virat@gmail.com>
2024-09-02 09:31:11 +0600

    agent profile and wallet added

--- app_auth/admin.py
+++ app_auth/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from app_auth.models.user import CustomUser
+from app_auth.models import user, wallet, agent_profile
+
 
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(user.CustomUser)
+admin.site.register(agent_profile.AgentProfile)
+admin.site.register(wallet.Wallet)
